7e435929c32ac7e0881e829156f68ea4630e2f6b
xciaoy <xcy1310@gmail.com>
2022-03-21 16:50:59 +0800

    update

--- src/components/FooterView.vue
+++ src/components/FooterView.vue
@@ -1,7 +1,7 @@
 <template>
   <footer class="bg-primary px-4 mt-5 text-center text-lighter">
     Copyright © 2021 - 2022, designed by
-    <a class="link-light text-decoration-none" href="https://2021.thef2e.com/users/6296427084285739330/">
+    <a class="link-light text-decoration-none" href="https://www.figma.com/file/nWH1xPWNWIhnqsuE1mNCsb/%E8%96%AA%E8%B3%87%E6%95%B8%E6%93%9A%E5%9C%96%E8%A1%A8%E8%AA%BF%E6%9F%A5?node-id=0%3A1">
     Chi
     </a>
     , made by
